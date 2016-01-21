06fc8f52685267c7a4820d3327c2af86d04ff8cf
filipeReitech <filipe@reitech.com.br>
2016-01-21 07:40:09 -0200

    adicionando jenkins com erro

---
+++ Jenkins/TesteJenkins/TesteJenkins/JenkinsTeste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TesteJenkins
+{
+    [TestClass]
+    public class JenkinsTeste
+    {
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(1, 1);
+            var qwr = rwq;
+            var 14 = 41;
+        }
+
+    }
+}
