d15bdd75a2636d6c4e13caea3a52875a11614709
filipeReitech <filipe@reitech.com.br>
2016-01-21 13:34:02 -0200

    adicionando testes corretamente

--- Jenkins/TesteJenkins/TesteJenkins/JenkinsTesteClass.cs
+++ Jenkins/TesteJenkins/TesteJenkins/JenkinsTesteClass.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace TesteJenkins
 {
-    [TestClass]
     public class JenkinsTesteClass
     {
 
-        [TestMethod]
         public void MyTestMethod()
         {
             //var abc = rw;
-            Assert.AreEqual(1, 1);
+            var ab = 2;
         }
 
     }
