a46ee8e1705f6f12d9a19888cf8c4d545e623241
filipeReitech <filipe@reitech.com.br>
2016-01-21 14:34:51 -0200

    removendo metodos

--- Jenkins/TesteJenkins/UnidadeTesteJenkins/UnitTest1.cs
+++ Jenkins/TesteJenkins/UnidadeTesteJenkins/UnitTest1.cs
@@ -12,41 +12,41 @@ namespace UnidadeTesteJenkins
             Assert.AreEqual(1, 1);
         }
 
-        [TestMethod]
-        public void MetodoComErro()
-        {
-            Assert.AreEqual(1, 2);
-        }
+        //[TestMethod]
+        //public void MetodoComErro()
+        //{
+        //    Assert.AreEqual(1, 2);
+        //}
 
-        [TestMethod]
-        public void MetodoComErro2()
-        {
-            Assert.AreEqual(1, 22);
-        }
+        //[TestMethod]
+        //public void MetodoComErro2()
+        //{
+        //    Assert.AreEqual(1, 22);
+        //}
 
-        [TestMethod]
-        public void MetodoComErro3()
-        {
-            Assert.AreEqual(1, 3);
-        }
+        //[TestMethod]
+        //public void MetodoComErro3()
+        //{
+        //    Assert.AreEqual(1, 3);
+        //}
 
-        [TestMethod]
-        public void MetodoComErro4()
-        {
-            Assert.AreEqual(1, 4);
-        }
+        //[TestMethod]
+        //public void MetodoComErro4()
+        //{
+        //    Assert.AreEqual(1, 4);
+        //}
 
-        [TestMethod]
-        public void MetodoSemErro2()
-        {
-            Assert.AreEqual(2, 2);
-        }
+        //[TestMethod]
+        //public void MetodoSemErro2()
+        //{
+        //    Assert.AreEqual(2, 2);
+        //}
 
-        [TestMethod]
-        public void MetodoSemErro3()
-        {
-            Assert.AreEqual(3, 3);
-        }
+        //[TestMethod]
+        //public void MetodoSemErro3()
+        //{
+        //    Assert.AreEqual(3, 3);
+        //}
 
     }
 }
