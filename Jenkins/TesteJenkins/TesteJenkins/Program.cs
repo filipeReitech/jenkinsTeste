180602b9e6701fab2affd10da87e580d1e95d552
filipeReitech <filipe@reitech.com.br>
2016-01-21 11:07:02 -0200

    erro

--- Jenkins/TesteJenkins/TesteJenkins/Program.cs
+++ Jenkins/TesteJenkins/TesteJenkins/Program.cs
@@ -16,6 +16,7 @@ namespace TesteJenkins
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var abc = afsf;
         }
     }
 }
