4e4bbd0eed0ba99ffabafb85e163cd6ca6a62407
filipeReitech <filipe@reitech.com.br>
2016-01-22 16:55:14 -0200

    adada

--- Jenkins/TesteJenkins/UnidadeTesteJenkins/ClasseTeste.cs
+++ Jenkins/TesteJenkins/UnidadeTesteJenkins/ClasseTeste.cs
@@ -19,11 +19,11 @@ namespace UnidadeTesteJenkins
             Assert.AreEqual(1, 1);
         }
 
-        [TestMethod]
-        public void MetodoComErro()
-        {
-            Assert.AreEqual(1, 2);
-        }
+        //[TestMethod]
+        //public void MetodoComErro()
+        //{
+        //    Assert.AreEqual(1, 2);
+        //}
 
         //[TestMethod]
         //public void MetodoComErro2()
